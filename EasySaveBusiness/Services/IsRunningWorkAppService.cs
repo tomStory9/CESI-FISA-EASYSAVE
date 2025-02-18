b0dd3dd70ffdb8b7cae18a4b6f890e9401fe968a
Tom Raineri <tomstory9@gmail.com>
2025-02-18 10:38:59 +0100

    renamed service

---
+++ EasySaveBusiness/Services/IsRunningWorkAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveBusiness.Services
+{
+    class IsRunningWorkAppService
+    {
+        public static bool IsRunning(string processName)
+        {
+            return Process.GetProcessesByName(processName).Length > 0;
+        }
+    }
+}
