fb54c9e3552e117739209699d7aacfbe9c700d7f
Simon Ledoux <simon@simon511000.fr>
2025-02-23 15:55:16 +0100

    feat: WIP add backup config wizard

---
+++ EasySaveDesktop/Views/CreateBackupConfigWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace EasySaveDesktop;
+
+public partial class CreateBackupConfigWindow : Window
+{
+    public CreateBackupConfigWindow()
+    {
+        InitializeComponent();
+    }
+}
